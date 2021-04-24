24f0d6af3ee2ac161e79d4ee4bceeebe1b1a0be7
meghna97 <meghna@codeweblabs.com>
2021-04-24 13:54:50 +0530

    new folder - arrays

---
+++ go/TDD/arrays/v7/sum.go
@@ -0,0 +1,26 @@
+package main
+
+//Sum calculates the total from a slice of numbers.
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+//SumAll calculates the respective sums of every slice passed in.
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
